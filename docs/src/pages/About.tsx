7d5a964923a9a65cf75adc6752eb6491f38ad85f
Miguecapi <zamiguelangel.zam@gmail.com>
2025-10-01 15:53:05 -0500

    about commit size of letter

--- docs/src/pages/About.tsx
+++ docs/src/pages/About.tsx
@@ -1,6 +1,6 @@
 export default function About() {
   return (
-    <section>
+    <section className="text-lg">
       <p className="text-gray-300 leading-relaxed">
         I am currently in my sixth semester of my Software Engineering program, which has allowed me to develop a solid foundation in programming principles, system design, and the development of technological solutions. I am looking for job opportunities that will allow me to apply my knowledge, gain professional experience, and grow within the field of software engineering.
       </p>
@@ -13,22 +13,20 @@ export default function About() {
         Although I haven't yet had formal employment under a contract, I have worked independently as a <span className="font-semibold text-white">freelancer</span>, participating in projects related to application development and digital solutions. During this time, I have gained practical experience in:
       </p>
 
-      <p className="mt-6 text-gray-300 leading-relaxed">
-        <ul className="list-disc list-inside space-y-2">
-          <li>
-            <span className="font-semibold text-white">Python with Django:</span> bug fixing and implementation of new features in existing applications.
-          </li>
-          <li>
-            <span className="font-semibold text-white">WordPress:</span> website redesign, customization, and performance optimization.
-          </li>
-          <li>
-            <span className="font-semibold text-white">APIs REST:</span> Enhancement and integration of services using Axios.
-          </li>
-          <li>
-            <span className="font-semibold text-white">Docker:</span> used in university projects for portability and environment deployment.
-          </li>
-        </ul>
-      </p>
+      <ul className="mt-6 text-gray-300 list-disc list-inside space-y-2">
+        <li>
+          <span className="font-semibold text-white">Python with Django:</span> bug fixing and implementation of new features in existing applications.
+        </li>
+        <li>
+          <span className="font-semibold text-white">WordPress:</span> website redesign, customization, and performance optimization.
+        </li>
+        <li>
+          <span className="font-semibold text-white">APIs REST:</span> Enhancement and integration of services using Axios.
+        </li>
+        <li>
+          <span className="font-semibold text-white">Docker:</span> used in university projects for portability and environment deployment.
+        </li>
+      </ul>
 
       <p className="mt-6 text-gray-300 leading-relaxed">
         My main goal is to join a team where I can actively contribute, apply and expand my knowledge, and continue my professional development. I am confident that my ability to learn, my work ethic, and my enthusiasm for technology will be a valuable asset in any work environment.
@@ -45,6 +43,7 @@ export default function About() {
         </a>
       </p>
     </section>
+
 
   );
 }
